5d9937b2ce299fd891116ae2b579c1fe8a0c785f
Namsang710 <86237789+Namsang710@users.noreply.github.com>
2022-06-30 01:52:39 +0900

    Section1 문제 5 풀이
    Section1 문제 5 풀이

---
+++ Inflearn_CodingTest/src/Section1/Problem5.java
@@ -0,0 +1,42 @@
+package Section1;
+
+import java.util.Scanner;
+
+public class Problem5 {
+
+    public String solution() {
+        Scanner s = new Scanner(System.in);
+
+        String str = s.next();
+        char[] chArray = str.toCharArray();
+
+        int lp = 0, rp = chArray.length - 1;
+
+        while(true){
+
+            if (lp > rp)
+                break;
+
+            if(Character.isLetter(chArray[lp]) && Character.isLetter(chArray[rp])){
+                char tmp = chArray[lp];
+                chArray[lp] = chArray[rp];
+                chArray[rp] = tmp;
+
+                rp--;
+                lp++;
+            }else if (Character.isLetter(chArray[lp]) || Character.isLetter(chArray[rp])){
+                if (Character.isLetter(chArray[lp])){
+                    rp--;
+                }else{
+                    lp++;
+                }
+            }else{
+                rp--;
+                lp++;
+            }
+        }
+        String answer = String.valueOf(chArray);
+        return answer;
+    }
+
+}
