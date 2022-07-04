37dc9201174d6883af7b97a78b0cb14fb10d381c
Namsang710 <86237789+Namsang710@users.noreply.github.com>
2022-07-04 23:54:53 +0900

    Section1 문제 10 수정 필요
    Section1 문제 10 수정 필요

--- Inflearn_CodingTest/src/Section1/Main.java
+++ Inflearn_CodingTest/src/Section1/Main.java
@@ -1,10 +1,24 @@
 package Section1;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+
+        String str = s.nextLine().toLowerCase(Locale.ROOT);
+        int distance[] = new int[str.length()];
+
+        for(int i=0; i<str.length(); i++){
+            distance[i] = 0;
+        }
+
+        for (int i=0; i<str.length(); i++){
+
+
+        }
+
     }
 }
