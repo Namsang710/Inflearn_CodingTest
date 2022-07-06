040e02c90a8383f9aeb4fd74ade854ee89668750
Namsang710 <nyh710@gmail.com>
2022-07-06 20:07:34 +0900

    Section1 문제 12 풀이
    Section1 문제 12 풀이 및 문자열 파트 종료

---
+++ Inflearn_CodingTest/src/Section1/Problem12.java
@@ -0,0 +1,29 @@
+package Section1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Problem12 {
+    public String solution() {
+        Scanner s = new Scanner(System.in);
+
+        int size = s.nextInt();
+        String input = s.next();
+        String answer = "";
+
+        ArrayList<String> secrets = new ArrayList();
+
+
+        for(int i=0; i<input.length(); i+= input.length() / size){
+            String str = input.substring(i, i + input.length() / size);
+            secrets.add(str);
+        }
+
+        for (String str : secrets){
+            int digit = Integer.parseInt(str.replaceAll("#", "1").replaceAll("\\*", "0"), 2);
+            answer += (char)digit;
+        }
+
+        return answer;
+    }
+}
