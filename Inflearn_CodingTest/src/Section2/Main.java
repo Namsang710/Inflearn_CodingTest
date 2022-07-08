47363842f687ed91d0e2b9dc058329050ee296ba
Namsang710 <nyh710@gmail.com>
2022-07-08 17:14:51 +0900

    Section2 문제 5 수정 필요
    Section2 문제 5 수정 필요

---
+++ Inflearn_CodingTest/src/Section2/Main.java
@@ -0,0 +1,12 @@
+package Section2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int size = s.nextInt();
+        
+    }
+}
