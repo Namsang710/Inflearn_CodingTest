47f7426a3110a1150cfe0a046c7b4cb13bb79bb4
Namsang710 <86237789+Namsang710@users.noreply.github.com>
2022-07-31 23:26:56 +0900

    Section4 문제 4풀이
    Section4 문제 4풀이

---
+++ Inflearn_CodingTest/src/Section4/Main.java
@@ -0,0 +1,20 @@
+package Section4;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        String str = s.next();
+        String t = s.next();
+
+        HashMap<Character, Integer> hashMap = new HashMap();
+        int answer = 0;
+        
+
+
+
+    }
+}
