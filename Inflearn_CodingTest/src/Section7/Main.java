e6ec991f93d2b127f4b96fef60d83b79a72afc24
Namsang710 <86237789+Namsang710@users.noreply.github.com>
2022-08-24 23:29:32 +0900

    Section7 문제 준비
    Section7 문제 준비

---
+++ Inflearn_CodingTest/src/Section7/Main.java
@@ -0,0 +1,11 @@
+package Section7;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+
+    }
+}
