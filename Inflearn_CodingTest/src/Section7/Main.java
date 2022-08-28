f7e56b146546b4bcb0c17513002626a1db202097
Namsang710 <86237789+Namsang710@users.noreply.github.com>
2022-08-28 21:59:00 +0900

    Section7 문제 8 풀이
    Section7 문제 8 풀이

--- Inflearn_CodingTest/src/Section7/Main.java
+++ Inflearn_CodingTest/src/Section7/Main.java
@@ -6,9 +6,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-
-
-
         int k = s.nextInt();
+
+        int arr[] = new int[n];
+
+        for(int i=0; i<n; i++)
+            arr[i] = s.nextInt();
+
+
+
     }
 }
