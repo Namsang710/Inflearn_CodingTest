92361fa181e4cd0c0016209959065b05e6664309
Namsang710 <86237789+Namsang710@users.noreply.github.com>
2022-09-17 23:03:13 +0900

    Section7 문제 3 풀이
    팩토리얼

---
+++ Inflearn_CodingTest/src/Section7/Main.java
@@ -0,0 +1,22 @@
+package Section7;
+
+import java.util.Scanner;
+
+public class Main {
+    public void solution() {
+        Scanner s = new Scanner(System.in);
+
+        int fact = DFS(s.nextInt());
+        System.out.println(fact);
+
+    }
+
+    public  int DFS(int n){
+        if (n==1)
+            return 1;
+        else{
+            return n * DFS(n - 1);
+        }
+
+    }
+}
