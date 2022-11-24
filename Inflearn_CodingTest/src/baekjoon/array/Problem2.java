31130cce4ea5e0708facddc3c52a8aa6d6edf12d
Namsang710 <nyh710@gmail.com>
2022-11-24 11:48:02 +0900

    백준 문제 풀이(이차원 배열)
    최댓값

---
+++ Inflearn_CodingTest/src/baekjoon/array/Problem2.java
@@ -0,0 +1,34 @@
+package baekjoon.array;
+
+import java.util.Scanner;
+
+public class Problem2 {
+    public void solution(){
+        Scanner sc = new Scanner(System.in);
+
+        int arr[][] = new int[9][9];
+
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int row = -1;
+        int col = -1;
+        int result = Integer.MIN_VALUE;
+
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if (result < arr[i][j]){
+                    row = i + 1;
+                    col = j + 1;
+                    result = arr[i][j];
+                }
+            }
+        }
+        System.out.println(result);
+        System.out.print(row + " " + col);
+        System.out.println();
+    }
+}
