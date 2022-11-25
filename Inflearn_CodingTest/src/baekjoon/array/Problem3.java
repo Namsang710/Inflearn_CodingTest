be3a18526fd4918596e27c41a5490b33aa37543e
Namsang710 <nyh710@gmail.com>
2022-11-25 09:48:38 +0900

    백준 문제 풀이(2차원 배열)
    색종이

---
+++ Inflearn_CodingTest/src/baekjoon/array/Problem3.java
@@ -0,0 +1,29 @@
+package baekjoon.array;
+
+import java.util.Scanner;
+
+public class Problem3 {
+    public void solution() {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int arr[][] = new int[100][100];
+
+
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            int w = sc.nextInt();
+            int h = sc.nextInt();
+
+            for(int j=h; j<h+10; j++){
+                for(int k=w; k<w+10; k++){
+                    if(arr[j][k] == 0) {
+                        arr[j][k] = 1;
+                        cnt++;
+                    }
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
