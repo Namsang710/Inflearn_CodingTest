71597366865e5b8d4fc90838892c788349f33d9b
Namsang710 <nyh710@gmail.com>
2022-12-07 17:30:45 +0900

    백준 문제풀이(투포인터)
    두 용액

---
+++ Inflearn_CodingTest/src/baekjoon/twopointer/Problem2.java
@@ -0,0 +1,52 @@
+package baekjoon.twopointer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Problem2 {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int lt = 0;
+        int rt = arr.length - 1;
+
+        int resultLt = -1;
+        int resultRt = -1;
+        int tmp = Integer.MAX_VALUE;
+
+        while(lt < rt){
+            int sum = arr[lt] + arr[rt];
+
+            if(Math.abs(sum) < Math.abs(tmp)){
+                tmp = sum;
+                resultLt = lt;
+                resultRt = rt;
+            }
+            if (sum > 0)
+                rt--;
+            else if(sum < 0)
+                lt++;
+            else{
+                resultLt = lt;
+                resultRt = rt;
+                break;
+            }
+        }
+
+        System.out.println(arr[resultLt] + " " + arr[resultRt]);
+    }
+}
